bda1dd9aa288ff25de6a2a6c2a5cb143485b5f71
JefersonT <jefersontorres.f@gmail.com>
2021-10-18 21:50:27 -0300

    first Commit

---
+++ src/com/br/DIO/PrimeiroPrograma.java
@@ -0,0 +1,30 @@
+package com.br.DIO;
+
+import com.br.DIO.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int a = 2;
+//		int b = 3;
+//		System.out.println("Hello word!" + (a + b));
+		
+		Gato gato = new Gato();
+		
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+		
+
+	}
+
+}
+
+class Livros {
+	private String name;
+	private String npag;
+}
